bd135de5451429bbb2c2ac5d9e0f4003f5c2eb34
Enzo Aramayo <enzosandoval@hotmail.com>
2021-06-09 19:50:34 -0300

    Beneficios para cliente terminado

--- src/main/java/ar/edu/unju/fi/tp9/service/imp/BeneficioServiceIpm.java
+++ src/main/java/ar/edu/unju/fi/tp9/service/imp/BeneficioServiceIpm.java
@@ -1,11 +1,9 @@
 package ar.edu.unju.fi.tp9.service.imp;
 
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 
 import ar.edu.unju.fi.tp9.entity.Beneficio;
 import ar.edu.unju.fi.tp9.repository.BeneficioRepository;
@@ -14,22 +12,25 @@ import ar.edu.unju.fi.tp9.service.IBeneficioService;
 @Service("beneficioServiceImp")
 public class BeneficioServiceIpm implements IBeneficioService {
 
-	
-@Autowired
-private BeneficioRepository beneficioRepository;
-	
-
+	@Autowired
+	private BeneficioRepository beneficioRepository;
 
 	@Override
 	public void guardar(Beneficio beneficio) {
-	 beneficioRepository.save(beneficio);
-		
+		beneficioRepository.save(beneficio);
+
 	}
 
 	@Override
 	public List<Beneficio> obtenerBeneficios() {
 		List<Beneficio> beneficios = beneficioRepository.findAll();
-		return beneficios;		
+		return beneficios;
+	}
+
+	@Override
+	public Beneficio buscarBeneficio(long id) {
+		Beneficio beneficio= beneficioRepository.findById(id).get();
+		return beneficio;
 	}
 
 }
